04d1689df91464a7a05c8702bf14aee3dcbfbda9
Brendan Smith <brendan@custommade.com>
2014-11-13 11:21:52 -0500

    creating setup.py for pypi install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='reuseable_runner',
+      version='0.1',
+      description='Test Runner to be used with django_jenkins that allows for database reuse',
+      url='http://github.com/brendancustommade/reuseable_runner',
+      author='brendan',
+      author_email='brendan@custommade.com',
+      license='MIT',
+      packages=['reuseable_runner'],
+      zip_safe=False)
